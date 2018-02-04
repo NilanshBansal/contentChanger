ceec025ab8ccdd107ea541d617d7da600b5b7f43
Nilansh Bansal <bansalnilansh@gmail.com>
2018-02-04 09:56:51 +0530

    replace Mr Trump with Crime Master Gogo

---
+++ content.js
@@ -0,0 +1,23 @@
+
+
+
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if( request.message === "clicked_browser_action" ) {
+        // alert("Hello from your Chrome extension!")
+/*         var firstHref = $("a[href^='http']").eq(0).attr("href");
+  
+        console.log(firstHref); */
+
+        $("body").children().each(function() {
+            $(this).html($(this).html().replace(/Donald Trump|Mr. Trump/gi,"Crime master Gogo"));
+        });
+
+        // chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+
+
+
+      }
+    }
+  );
